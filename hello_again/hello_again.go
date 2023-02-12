f131a4f682be686449e428bd17c78b2ba8a9f28d
zyzyx <fauzuwan.nazri93@gmail.com>
2023-02-13 00:51:32 +0800

    refactoring test

--- hello_again/hello_again.go
+++ hello_again/hello_again.go
@@ -5,6 +5,9 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func HelloAgain(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello_again/hello_again_test.go
+++ hello_again/hello_again_test.go
@@ -6,17 +6,18 @@ func TestHelloAgain(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := HelloAgain("Zyzyx")
 		want := "Hello, Zyzyx"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello' when an empty string is supplied", func(t *testing.T) {
 		got := HelloAgain("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
